f365b9b569234deaf63bf9eda18497eecffd604b
Peter Reuterås <peter@reuteras.com>
2010-11-18 01:00:05 +0100

    fixed an error with tweet id, now id_str and added test

--- later.pl
+++ later.pl
@@ -5,28 +5,30 @@
 $addaction = sub {
 	my $command = shift;
 
-	if ($command =~ m#^/later (.+) (.+)#) {
+  	if ($command =~ m#^/later ([^ ]+) ([^ ]+)( (.+))?#) {
 		my $tweet_id=$1;
 		my $desc=$2;
-		my $tweet = &get_tweet($tweet_id);
-		if (!$tweet->{'id'}) {
-			print $stdout "-- sorry, no such tweet (yet?).\n";
-			return 1;
+		my $testing=$4;
+		my $tweet=&get_tweet($tweet_id);
+		if (!$tweet->{'id_str'}) {
+		    print $stdout "-- sorry, no such tweet (yet?).\n";
+		    return 1;
 		}
 		chop( my $del_pass=`cat ~/.delicious`);
 		my $add_url="http://twitter.com/$tweet->{'user'}->{'screen_name'}/statuses/$tweet->{'id'}";
-		my $del_url="https://api.del.icio.us/v1/posts/add?url=" .
+		my $delicious_url="https://api.del.icio.us/v1/posts/add?url=" .
 			$add_url . "&tags=\@ttytter_added\@&shared=no&description=$desc";
-		my $content = `curl -s -u \"$del_pass\" \"$del_url\"`;
+		my $content = `curl -s -u \"$del_pass\" \"$delicious_url\"`;
 		if ($content =~ m!<result code="done" />!){
 			print "Added to delicious.com\n";
-		}else{
+			if ($testing == "hudson-test"){
+				`echo "$delicious_url" > /tmp/hudson-ttytter-later-test`;
+			}
+		}else{ 
 			print "Something went wrong, not added. Se response:\n";
-			print $content;	
+			print $content;
 		}
 		return 1;
 	}
-
 	return 0;
 };
-
