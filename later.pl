b83108711ce2b515913ced51945bea2f6899565d
jdvalentine <jdvalentine@gmail.com>
2011-01-12 00:27:12 +0000

    This version of the /later command requires that you create ~/.instapaper and place your instapaper.com username in it. You cannot at this stage give a password, as I must admit that I can't seem to get HTTP basic auth working, so I'm passing the username alone.

--- later.pl
+++ later.pl
@@ -2,32 +2,32 @@
 # viewing from GUI browser
 #
 
+use URI::Escape;
+
 $addaction = sub {
 	my $command = shift;
 
-  	if ($command =~ m#^/later ([^ ]+) ([^ ]+)( (.+))?#) {
+  	if ($command =~ m#^/later ([^ ]+) ?#) {
 		my $tweet_id=$1;
-		my $desc=$2;
-		my $testing=$4;
 		my $tweet=&get_tweet($tweet_id);
 		if (!$tweet->{'id_str'}) {
 		    print $stdout "-- sorry, no such tweet (yet?).\n";
 		    return 1;
 		}
-		chop( my $instapaper_pass=`cat ~/.instapaper`);
-		my $add_url="http://twitter.com/$tweet->{'user'}->{'screen_name'}/statuses/$tweet->{'id'}";
-		my $instapaper_url="https://www.instapaper.com/api/add?url=" .
-			$add_url . "&tags=\@ttytter_added\@&shared=no&description=$desc";
-		my $content = `curl -s -u \"$instapaper_pass\" \"$instapaper_url\"`;
-        print $instapaper_url;
-        print $content;
-		if ($content =~ m!<result code="done" />!){
-			print "Added to instapaper.com\n";
+        
+		chop( my $instapaper_user=`cat ~/.instapaper`);
+		my $add_url=uri_escape("http://twitter.com/$tweet->{'user'}->{'screen_name'}/statuses/$tweet->{'id_str'}");
+       
+		my $instapaper_url="https://www.instapaper.com/api/add?username=$instapaper_user&url=" .  $add_url;
+		my $content = `curl -s \"$instapaper_url\"`;
+        
+		if ($content =~ m!201!){
+			print "\nAdded to instapaper.com\n";
 			if ($testing == "hudson-test"){
 				`echo "$add_url" > hudson-ttytter-later-test`;
 			}
 		}else{ 
-			print "Something went wrong, not added. Se response:\n";
+			print "Something went wrong, not added. Response: ";
 			print $content;
 		}
 		return 1;
